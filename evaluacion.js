a61d0467918b96f847ef615deaee91364c45b8b7
revelo2026-ux <revelo2026@gmail.com>
2026-07-24 08:56:37 -0500

    feature:se agrega el ultimo ejercicio

--- evaluacion.js
+++ evaluacion.js
@@ -3,8 +3,8 @@ let precio1 = 4.5,precio2=1.3;
 let producto1 = true;
 let producto2 = false;
 
-console.log(`El precio de ${nombre1} es de: ${precio2}`);
-console.log(`El precio de ${nombre2} es de: ${precio1}`);
+console.log(`El precio de ${nombre1} es de: ${precio2} y esta ${producto1} en stock`);
+console.log(`El precio de ${nombre2} es de: ${precio1} y esta${producto2} en stock`);
 
 
 let precio3=5;
@@ -41,3 +41,16 @@ for(let i=1;i<=20;i++){
     acum=acum++; 
 }
 console.log(`La suma total es de:${acum}`);
+
+let mayor=0;
+function saber(num1,num2){
+    if(num1>mayor){
+        console.log(`El numero ${num1} es mayor`);
+        mayor=num1;
+    }else if(mayor>num2){
+        console.log(`El numero ${num2} es mayor`);
+    }
+
+}
+
+saber(6,5);
