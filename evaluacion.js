9881d8c7ffd83c3b3f7a323873a9f0e7b749b79f
revelo2026-ux <revelo2026@gmail.com>
2026-07-24 07:43:59 -0500

    feature:se agrego el archivo JS y se imprime el primer ejercicio

---
+++ evaluacion.js
@@ -0,0 +1,7 @@
+let nombre1 = "leche",nombre2="switch";
+let precio1 = 4.5,precio2=1.3;
+let producto1 = true;
+let producto2 = false;
+
+console.log(`El precio de ${nombre1} es de: ${precio2}`);
+console.log(`El precio de ${nombre2} es de: ${precio1}`);
